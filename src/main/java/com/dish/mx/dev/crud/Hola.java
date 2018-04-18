be1598cf1d5f229e415eeabbfe1ea37533b39ae9
gjmf1109 <38501053+gjmf1109@users.noreply.github.com>
2018-04-18 10:21:59 -0500

    Se creo la clase Hola
    Por cuestión de práctica

---
+++ src/main/java/com/dish/mx/dev/crud/Hola.java
@@ -0,0 +1,10 @@
+public class Hola{
+
+  public static void holaMundo(){
+    System.out.println("Hola Mundo");
+  }
+  
+  public static void holaUsuario(String nombre){
+    System.out.println("Hola " + nombre);
+  }    
+}
